2941e9dc85ecf064f2f7c1978f0ca1bc8db87d03
zacktzp <zackteo94@gmail.com>
2020-03-20 15:41:59 +0800

    Method get
    get long lat from bar.  but haven change the variable in other files,

--- GUI.py
+++ GUI.py
@@ -7,14 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('user.html')
 
-# @app.route("/", methods=["GET", "POST"])
-# def need_input():
-#     for key, value in request.form.items():
-#         print("key: {0}, value: {1}".format(key, value))
-#
-# @app.route("/form", methods=["GET"])
-# def get_form():
-#     return render_template('user.html')
+@app.route("/", methods=["GET", "POST"])
+def need_input():
+    for key, value in request.form.items():
+        start = request.form.items.get("start", "")
+        end = request.form.items.get("end", "")
+        print("key: {0}, value: {1}".format(key, value))
+    return start,end
+
+@app.route("/form", methods=["GET"])
+def get_form():
+    return render_template('user.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
